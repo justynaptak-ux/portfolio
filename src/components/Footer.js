0f259220f14ba203dc0314318ad2a999232a2043
joannakochel <joanna@otainsight.com>
2020-06-29 14:57:43 +0200

    text corrections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className="footercontainer">
       <h5 className="footerdescription">
-        <b>Let's talk,</b> <br />I am open to any development opportunity. I put
-        all my involvement in my work, and here are the effects.
+        <b>Let's talk,</b> <br />I am open to any UX/UI or web development opportunity. <br />
+        I am always very involved in my projects and put a lot of effort in them. <br /> You can see the results on this site.
       </h5>
       <h6 className="mail">
         e-mail: justyna.ptak.ux@gmail.com <br />
